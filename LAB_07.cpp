c27d06e0dcf22f43208213aa990f63d5e5177847
DawidSzoka1 <oficer130@gmail.com>
2024-04-17 13:39:02 +0200

    zad 1

---
+++ LAB_07.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Figura{
+protected:
+    string typ;
+    double pole;
+    double obwod;
+public:
+    void wypisz(){
+        cout << "ta figura to " << typ << " o obwodzie " << obwod << ", polu " << pole << endl;
+    }
+
+};
+class Kolo: public Figura{
+private:
+    double promien;
+public:
+    double policzPole(){
+        return promien * promien * 3.14;
+    }
+    double policzObwod(){
+        return 2*promien * 3.14;
+    }
+    Kolo(double promien): promien(promien){
+        typ = "kolo";
+        pole = policzPole();
+        obwod = policzObwod();
+    }
+};
+
+class Kwadrat: public Figura{
+private:
+    double bok;
+public:
+    double policzPole(){
+        return bok * bok;
+    }
+    double policzObwod(){
+        return 4*bok;
+    }
+    Kwadrat(double bok):bok(bok){
+        typ = "kwadrat";
+        pole = policzPole();
+        obwod = policzObwod();
+    }
+};
+
+class Prostokat: public Figura{
+private:
+    double a;
+    double b;
+public:
+    double policzPole(){
+        return a*b;
+    }
+    double policzObwod(){
+        return 2*a + 2*b;
+    }
+    Prostokat(double a, double b):a(a), b(b){
+        typ = "prostokat";
+        pole = policzPole();
+        obwod = policzObwod();
+    }
+};
+
+int main(){
+
+    return 0;
+}
