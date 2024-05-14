9eec828bc3a7633ff59512e8116d6417e16f2fcc
DawidSzoka1 <oficer130@gmail.com>
2024-05-14 09:31:29 +0200

    vector cpp

---
+++ LAB_10.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<time.h>
+
+
+using namespace std;
+
+void wyswietl(vector <int> vec1){
+    for(int i = 0; i<vec1.size(); i++){
+        cout << vec1.at(i) << endl;
+    }
+}
+
+int main(){
+    srand(time(NULL));
+    vector <int> lista;
+
+    for(int i = 0; i<10; i++){
+        lista.push_back(rand() % 100 + 1);
+    }
+    wyswietl(lista);
+  //  lista.erase(lista.begin()+3);
+  //lista.erase(lista.begin());//usuwa pierwszy jak damy + 4 to usunie 5 element
+   // lista.erase(lista.end()); //ostatni
+   // lista.erase(lista.begin(), lista.begin()+2);//usuwa przedzial
+   // lista.insert(lista.begin()+3, 550);//wstawia wartosc
+   // wyswietl(lista);
+    cout << "Nowy sposob" << endl;
+    for(int x: lista){
+        cout << x << endl;
+    }
+    cout<<"ITERATORE" << endl;
+    vector <int>:: iterator it;
+    for(it = lista.begin(); it != lista.end(); it++){
+        if(*it % 2 == 0){
+                cout << *it << endl;
+        }
+    }
+    // #include<alponthn>
+    // sortowanie sort(vec.begin(), vec.end());
+
+    return 0;
+}
