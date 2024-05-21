dad4adf6635af1c1233883a65cdbf742c2dee41f
DawidSzoka1 <oficer130@gmail.com>
2024-05-21 09:32:56 +0200

    sort objectcs

---
+++ LAB_11.cpp
@@ -0,0 +1,19 @@
+#include<string>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Miasto{
+public:
+    int liczba;
+    string nazwa;
+    Miasto(){}
+    Miasto(int liczba, string nazwa): liczba(liczba), nazwa(nazwa){}
+};
+
+int main(){
+
+
+    return 0;
+}
